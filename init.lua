5cd8dc8ec7de3baed2db08539b72daa1e38a6d66
alexandreIorio <alexandre.iorio@heig-vd.ch>
2024-11-24 18:55:03 +0100

    remove useless plugin

--- init.lua
+++ init.lua
@@ -263,20 +263,15 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   'github/copilot.vim',
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
-  {
-    'tadmccorkle/markdown.nvim',
-    ft = 'markdown', -- or 'event = "VeryLazy"'
-    opts = {
-      -- configuration here or empty for defaults
-    },
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install && git restore .',
+    -- or if you use yarn: (I have not checked this, I use npm)
+    -- build = "cd app && yarn install && git restore .",
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
   },
   {
     'GustavEikaas/easy-dotnet.nvim',
